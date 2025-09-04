2cad85f0965c2ee7032fa65f3765a5278865ef53
Noel <r25626171@gmail.com>
2025-09-04 20:30:14 +0900

    v1.2 if else

--- week04/src/PoketmonGame.java
+++ week04/src/PoketmonGame.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Poketmon {
     //concrete class (can produce object)
     int hp;
@@ -6,11 +8,20 @@ class Poketmon {
 public class PoketmonGame {
     public static void main(String[] args) {
         Poketmon pikachu = new Poketmon();
-        Poketmon squirtle = new Poketmon();
-        Poketmon poketmon = new Poketmon();
-        pikachu.hp=100;
-        squirtle.hp=110;
-        System.out.println(pikachu.hp);
-        System.out.println(squirtle.hp);
+        //Poketmon squirtle = new Poketmon();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("체력을 입력하세요 : ");
+        pikachu.hp= scanner.nextInt();
+        //squirtle.hp=110;
+        //System.out.println(pikachu.hp);
+        //System.out.println(squirtle.hp);
+
+        if(pikachu.hp>99) {
+            System.out.println("피카츄의 체력("+pikachu.hp+")이 여유!");
+        }
+        else {
+            System.out.println("피카츄의 체력("+pikachu.hp+")이 부족!");
+        }
     }
 }
